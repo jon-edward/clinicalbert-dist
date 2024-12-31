181dd9cef3cf2fc171fa38ded19d4c1278b047bc
jon-edward <townsend.jonathan.e@gmail.com>
2024-12-30 19:10:53 -0500

    Combine directories

--- build.py
+++ build.py
@@ -5,26 +5,21 @@ import transformers
 
 MODEL_IDENT = "emilyalsentzer/Bio_ClinicalBERT"
 
-MODEL_DIR = pathlib.Path(__file__).parent / "bio_clinicalbert-model"
-TOKENIZER_DIR = pathlib.Path(__file__).parent / "bio_clinicalbert-tokenizer"
+OUT_DIR = pathlib.Path(__file__).parent / "bio_clinicalbert"
 
 
 def build():
 
-    if MODEL_DIR.exists():
-        shutil.rmtree(MODEL_DIR)
+    if OUT_DIR.exists():
+        shutil.rmtree(OUT_DIR)
 
-    if TOKENIZER_DIR.exists():
-        shutil.rmtree(TOKENIZER_DIR)
+    model = transformers.AutoModel.from_pretrained(OUT_DIR)
+    model.save_pretrained(OUT_DIR)
 
-    model = transformers.AutoModel.from_pretrained(MODEL_IDENT)
-    model.save_pretrained(MODEL_DIR)
+    tokenizer = transformers.AutoTokenizer.from_pretrained(OUT_DIR)
+    tokenizer.save_pretrained(OUT_DIR)
 
-    tokenizer = transformers.AutoTokenizer.from_pretrained(MODEL_IDENT)
-    tokenizer.save_pretrained(TOKENIZER_DIR)
-
-    shutil.make_archive(MODEL_DIR.name, "zip", MODEL_DIR)
-    shutil.make_archive(TOKENIZER_DIR.name, "zip", TOKENIZER_DIR)
+    shutil.make_archive(OUT_DIR.name, "zip", OUT_DIR)
 
 if __name__ == "__main__":
     build()
