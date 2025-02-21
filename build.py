774cc678656f60d31b2418c29d64b27224075ec9
jon-edward <townsend.jonathan.e@gmail.com>
2025-02-21 07:06:39 -0500

    Add bioclinicalbert_qa

--- build.py
+++ build.py
@@ -3,23 +3,26 @@ import shutil
 
 import transformers
 
-MODEL_IDENT = "emilyalsentzer/Bio_ClinicalBERT"
+out_root = pathlib.Path(__file__).parent
 
-OUT_DIR = pathlib.Path(__file__).parent / "bio_clinicalbert"
+models = [
+    ("emilyalsentzer/Bio_ClinicalBERT", out_root / "bio_clinicalbert"),
+    ("jon-t/Bio_ClinicalBERT_QA", out_root / "bio_clinicalbert_qa"),
+]
 
 
 def build():
+    for model_ident, out_dir in models:
+        if out_dir.exists():
+            shutil.rmtree(out_dir)
 
-    if OUT_DIR.exists():
-        shutil.rmtree(OUT_DIR)
+        model = transformers.AutoModel.from_pretrained(model_ident)
+        model.save_pretrained(out_dir)
 
-    model = transformers.AutoModel.from_pretrained(MODEL_IDENT)
-    model.save_pretrained(OUT_DIR)
+        tokenizer = transformers.AutoTokenizer.from_pretrained(model_ident)
+        tokenizer.save_pretrained(out_dir)
 
-    tokenizer = transformers.AutoTokenizer.from_pretrained(MODEL_IDENT)
-    tokenizer.save_pretrained(OUT_DIR)
-
-    shutil.make_archive(OUT_DIR.name, "zip", OUT_DIR)
+        shutil.make_archive(out_dir.name, "zip", out_dir)
 
 if __name__ == "__main__":
     build()
